d75406f2a882698c410072ac4a1de29fafcdb088
yipengh <huang.yipeng@hotmail.com>
2022-06-07 12:36:38 +0000

    Fix build-in-docker.sh removing `-it`

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-docker run --rm -it --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data blang/latex:ubuntu make
+docker run --rm --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data blang/latex:ubuntu make
